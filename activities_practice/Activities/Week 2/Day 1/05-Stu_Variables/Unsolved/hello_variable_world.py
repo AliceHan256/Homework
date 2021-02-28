d99e4f75b07e3d5861bc765aebf31f41d3ce2a60
AliceHan <alicehan.doc@gmail.com>
2021-02-28 17:30:15 +1100

    added python-homework file

--- activities_practice/Activities/Week 2/Day 1/05-Stu_Variables/Unsolved/hello_variable_world.py
+++ activities_practice/Activities/Week 2/Day 1/05-Stu_Variables/Unsolved/hello_variable_world.py
@@ -5,16 +5,16 @@
 # Percent Increase = Increase / Original x 100
 
 # Create float variable for original_price
-
+original_price = 198.87
 
 # Create float variable for current_price
-
+current_price = 254.32
 
 # Calculate difference between current_price and original_price
-
+increase = current_price - original_price
 
 # Calculate percent_increase
-
+percent_increase = (increase / original_price) * 100
 
 # Print original_price
 print(f"Apple's original stock price was ${original_price}")
@@ -23,6 +23,7 @@ print(f"Apple's original stock price was ${original_price}")
 print(f"Apples current stock price is ${current_price}")
 
 # Print percent_increase
-
+print(f"Apple's stock price increased by ${percent_increase}")
 # CHALLENGE:
 # Use a format specifier with the f-string to print the percent increase with two decimal places:
+print(f"Apple's stock price increased by {percent_increase:.2f}%")
